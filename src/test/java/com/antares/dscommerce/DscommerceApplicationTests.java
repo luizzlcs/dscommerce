296629dfd4ca5287628a9250729f9a0c3aca87ce
Luiz Carlos <luizzlcs@gmail.com>
2024-11-21 09:17:19 -0300

    Created project DsCommerce

---
+++ src/test/java/com/antares/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antares.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
