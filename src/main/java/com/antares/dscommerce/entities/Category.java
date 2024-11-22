4fcea9eaec0d10d99ac24287a535eb0084e637e2
Luiz Carlos <luizzlcs@gmail.com>
2024-11-21 22:02:38 -0300

    Equals HashCode

--- src/main/java/com/antares/dscommerce/entities/Category.java
+++ src/main/java/com/antares/dscommerce/entities/Category.java
@@ -22,7 +22,7 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
-    public Category(){
+    public Category() {
 
     }
 
@@ -51,8 +51,29 @@ public class Category {
         return products;
     }
 
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-    
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Category other = (Category) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
