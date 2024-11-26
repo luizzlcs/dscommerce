44ec6c7779807d4d2709237aaa40176705bb2d68
Luiz Carlos <luizzlcs@gmail.com>
2024-11-26 16:42:13 -0300

    Buscando os produtos com paginação.

--- src/main/java/com/antares/dscommerce/controller/ProductController.java
+++ src/main/java/com/antares/dscommerce/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.antares.dscommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,15 @@ public class ProductController {
 
 	@GetMapping(value = "/{id}")
 	public ProductDTO findById(@PathVariable Long id) {
+		
 		ProductDTO dto = services.findById(id);
 
 		return dto;
+	}
+
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return services.findAll(pageable);
 	}
 
 }
--- src/main/java/com/antares/dscommerce/services/ProductServices.java
+++ src/main/java/com/antares/dscommerce/services/ProductServices.java
@@ -3,6 +3,8 @@ package com.antares.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +19,17 @@ public class ProductServices {
     ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public ProductDTO findById(Long id){
+    public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
-    
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
 }
