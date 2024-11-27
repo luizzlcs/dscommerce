d131a48604a84c3d4cee716d5e5656d2617307a1
Luiz Carlos <luizzlcs@gmail.com>
2024-11-27 11:35:10 -0300

    Customizando resposta com ResponseEntity e PUT product

--- src/main/java/com/antares/dscommerce/controller/ProductController.java
+++ src/main/java/com/antares/dscommerce/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.antares.dscommerce.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.antares.dscommerce.dto.ProductDTO;
 import com.antares.dscommerce.services.ProductServices;
@@ -21,21 +26,33 @@ public class ProductController {
 	private ProductServices services;
 
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
-		
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+
 		ProductDTO dto = services.findById(id);
 
-		return dto;
+		return ResponseEntity.ok(dto);
 	}
 
 	@GetMapping
-	public Page<ProductDTO> findAll(Pageable pageable) {
-		return services.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		Page<ProductDTO> dto = services.findAll(pageable);
+		return ResponseEntity.ok(dto);
 	}
 
 	@PostMapping
-	public ProductDTO insert(@RequestBody ProductDTO dto) {
-		return services.Insert(dto);
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = services.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{if}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+
+		dto = services.update(id, dto);
+
+		return ResponseEntity.ok(dto);
 	}
 
 }
--- src/main/java/com/antares/dscommerce/entities/OrderItem.java
+++ src/main/java/com/antares/dscommerce/entities/OrderItem.java
@@ -3,9 +3,6 @@ package com.antares.dscommerce.entities;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
