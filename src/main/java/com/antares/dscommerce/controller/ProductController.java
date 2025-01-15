337b42db6feb6ef7cb2c9bd85d8345d0d07a8dd4
Luiz Carlos <luizzlcs@gmail.com>
2025-01-15 18:22:47 -0300

    Fazer o controle de acesso por perfil e rota

--- src/main/java/com/antares/dscommerce/controller/ProductController.java
+++ src/main/java/com/antares/dscommerce/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ public class ProductController {
 	@Autowired
 	private ProductServices services;
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 
@@ -43,7 +45,8 @@ public class ProductController {
 		Page<ProductDTO> dto = services.findAll(name, pageable);
 		return ResponseEntity.ok(dto);
 	}
-
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = services.insert(dto);
@@ -52,6 +55,7 @@ public class ProductController {
 		return ResponseEntity.created(uri).body(dto);
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
 					
@@ -60,6 +64,7 @@ public class ProductController {
 		
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 
