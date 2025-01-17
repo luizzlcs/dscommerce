6822bd46029c1bcf03c7c330b8e277093097b1a1
Luiz Carlos <luizzlcs@gmail.com>
2025-01-17 15:48:56 -0300

    Get user logged

---
+++ src/main/java/com/antares/dscommerce/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.antares.dscommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.antares.dscommerce.dto.UserDTO;
+import com.antares.dscommerce.services.UserService;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService services;
+
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+	@GetMapping(value = "/me")
+	public ResponseEntity<UserDTO> getMe() {
+		UserDTO dto = services.getMe();
+		return ResponseEntity.ok(dto);
+	}
+
+	
+
+}
--- src/main/java/com/antares/dscommerce/services/UserService.java
+++ src/main/java/com/antares/dscommerce/services/UserService.java
@@ -3,15 +3,21 @@ package com.antares.dscommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.antares.dscommerce.dto.UserDTO;
 import com.antares.dscommerce.entities.Role;
 import com.antares.dscommerce.entities.User;
 import com.antares.dscommerce.projection.UserDetailsProjection;
 import com.antares.dscommerce.repositories.UserRepository;
+
 
 @Service
 public class UserService implements UserDetailsService {
@@ -34,6 +40,27 @@ public class UserService implements UserDetailsService {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return repository.findByEmail(username).get();
+
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("User not found Exception");
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
+
     }
 
 }
