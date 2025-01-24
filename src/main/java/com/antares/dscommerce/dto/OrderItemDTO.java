c147752eb1f2d53efe2f315113230d7a4439ab20
Luiz Carlos <luizzlcs@gmail.com>
2025-01-24 12:51:11 -0300

    Add return of the url image

--- src/main/java/com/antares/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/antares/dscommerce/dto/OrderItemDTO.java
@@ -7,12 +7,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
     
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -20,6 +22,7 @@ public class OrderItemDTO {
         name = entity.geProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
+        imgUrl = entity.geProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -41,5 +44,11 @@ public class OrderItemDTO {
     public Double getSubTotal(){
         return quantity * price;
     }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    
     
 }
