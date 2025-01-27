fb83f037331bc5653bb311152743ddc1b11ff6a2
Luiz Carlos <luizzlcs@gmail.com>
2025-01-27 11:15:32 -0300

    Fix validation

--- src/main/java/com/antares/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/antares/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.antares.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -19,9 +20,13 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo é requerido")
     private String description;
+    @NotNull(message = "Campo é requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
+
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    public List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -41,8 +46,7 @@ public class ProductDTO {
             categories.add(new CategoryDTO(cat));
         }
     }
-    @NotEmpty(message = "Deve ter pelo menos uma categoria")
-    public List<CategoryDTO> categories = new ArrayList<>();
+    
 
     public Long getId() {
         return id;
